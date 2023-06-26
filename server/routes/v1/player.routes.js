2ec1f090755f5c5ffca02d49886ea10d37e366a5
Hirzi Nurfakhrian <hirzinurfakhrian@gmail.com>
2023-06-26 21:05:08 +0700

    add 1st endpoint document

--- server/routes/v1/player.routes.js
+++ server/routes/v1/player.routes.js
@@ -5,6 +5,37 @@ const playerRouter = require("express").Router();
  * @Routes "/api/v1/players"
  */
 
+/**
+ * Response
+ * @typedef {object} Response
+ * @property {string} result - The title
+ * @property {array<Player>} data - The content
+ */
+
+/**
+ * GET /players
+ * @summary List all players
+ * @description Listing all players from the database
+ * @operationId listPlayers
+ * @return {Response} 200 - success response
+ * @return {object} 400 - failed response
+ * @example response - 200 - example success response
+ * {
+ *   "result": "Success",
+ *   "data": [
+ *     {
+ *       "id": 1,
+ *       "username": "galileo",
+ *       "email": "galileo@brazil.com",
+ *       "password": "$2a$10$Q.uMt79272XWj2DmFBgGEuevlRMM4oEPjmFL122nacdqwKMt5hA1u\n",
+ *       "experience": 0,
+ *       "lvl": 0,
+ *       "createdAt": "2023-06-25T17:00:00.000Z",
+ *       "updatedAt": "2023-06-25T17:00:00.000Z"
+ *     }
+ *   ]
+ * }
+ */
 playerRouter.get("/", PlayerController.getPlayers);
 playerRouter.post("/", PlayerController.createPlayer);
 playerRouter.get("/:id", PlayerController.getPlayerById);
